4f0408c49387d20acb1f69520f3aa0e1fcccaf3c
Brittany Morris <brittmmendez@gmail.com>
2018-02-22 01:40:25 +0000

    automatically backed up by learn

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -26,8 +26,7 @@ class RecipesController < ApplicationController
   end
 
   def update
-#@recipe=Recipe.find(params[:id])
-
+    @recipe=Recipe.find(params[:id])
      if @recipe.update(recipe_params)
        redirect_to @recipe
      else
